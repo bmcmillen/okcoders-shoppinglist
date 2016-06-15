75770fc68f146245c07061e745ccb3e9a68efaac
Brian McMillen <admin@john316mission.org>
2016-06-14 19:46:59 -0500

    Basic framework of our webapp

---
+++ node/shoppinglist.js
@@ -0,0 +1,14 @@
+// Main js file of our shoppinglist app
+var express = require('express');   //Import express routing module
+
+var app = express();                // Create our app object that holds routes
+
+app.listen(3000, function() {       // Start listening on port 3000
+  console.log('listening on http://localhost:3000');
+});
+
+app.use('/', express.static('./app'));   // Serve our static HTML files
+
+app.get('/list', function(req,res) {
+  res.json([1,2,3]);
+});
